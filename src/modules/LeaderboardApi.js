9d87d0baeecf869277382e7d33041aa34d78e51d
Saadat123456 <saadatlucky313@gmail.com>
2022-03-03 21:04:00 +0500

    Get All the data of the game and load into DOM

--- src/modules/LeaderboardApi.js
+++ src/modules/LeaderboardApi.js
@@ -26,6 +26,17 @@ class LeaderboardApi {
       'game-id': data.result.split(' ')[3],
     });
   }
+
+  getGameData = async () => {
+    const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${this.gameId}/scores/`, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    const data = await response.json();
+    this.loadDataToDocument(data.result);
+  }
 }
 
 export default LeaderboardApi;
