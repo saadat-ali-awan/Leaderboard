ddf161d99b1fd96e3fc72df39ebbeb330ef83f99
Saadat123456 <saadatlucky313@gmail.com>
2022-03-03 21:05:54 +0500

    Create the main app controller.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+import LeaderboardApi from './modules/LeaderboardApi.js';
 import './style.css';
+
+const leaderboardApi = new LeaderboardApi();
+leaderboardApi.init();
+
+window.addEventListener('load', () => {
+  const nameInput = document.querySelector('#name');
+  const scoreInput = document.querySelector('#score');
+  window.addEventListener('popstate', () => {
+    switch (window.location.hash) {
+      case '#submit-score':
+        if (nameInput.value !== '' && scoreInput.value !== '') {
+          leaderboardApi.addPlayerData(nameInput.value, scoreInput.value);
+        }
+        break;
+      case '#refresh':
+        leaderboardApi.getGameData();
+        break;
+      default:
+        break;
+    }
+  });
+});
